5c1c1adda0a76abf561c3231469ba56246bf2f92
chetta chetta chetta <>
2019-07-06 12:29:51 +0700

    finish #15 create form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { AppURL } from '../../app.url';
+import { IRegisterComponent } from './register.interface';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements IRegisterComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private builder: FormBuilder) {
+    this.initialCreateFromData()
   }
 
   Url = AppURL
+  form: FormGroup;
+
+  // ลงทะเบียน
+  onSubmit() {
+    console.log(this.form.value)
+  }
+
+  private initialCreateFromData()
+  {
+      //สร้างฟอร์ม
+      this.form  = this.builder.group({
+        firstname: [],
+        lastname: [],
+        email: [],
+        password: [],
+        cpassword: []
+     })
+  }
 }

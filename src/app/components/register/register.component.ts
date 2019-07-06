b0c32aee45da65343d4bc147bba544aca966b9fb
chetta chetta chetta <>
2019-07-06 13:12:53 +0700

    finish #17 Register page create own validator

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppURL } from '../../app.url';
 import { IRegisterComponent } from './register.interface';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 import { AlertService } from '../../shareds/services/alert.service';
 
 declare let $;
@@ -38,9 +38,25 @@ export class RegisterComponent implements IRegisterComponent {
          lastname: ['', [Validators.required]],
          email: ['', [Validators.required]],
          password: ['', [Validators.required]],
-         cpassword: ['', [Validators.required]]
+         cpassword: ['', [Validators.required, this.comparePassword('password')]]
       })
   }
 
   // สร้างรายละเอียดเอง
+  private comparePassword(passwordField: string) {
+    return function(confirm_password: AbstractControl) {
+      if(!confirm_password.parent) return
+
+      const password = confirm_password.parent.get(passwordField)
+
+      const passwordSubscribe = password.valueChanges.subscribe(() => {
+         confirm_password.updateValueAndValidity()
+         passwordSubscribe.unsubscribe()
+      })
+
+      if(confirm_password.value === password.value) return;
+
+      return { compare: true }
+    }
+  }
 }
